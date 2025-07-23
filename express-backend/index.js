b82b3da391c304f1cfe18d7a2b1ddc6728ff4fb3
RakaValrizqy <pendragonqword@gmail.com>
2025-07-23 22:45:45 +0700

    menambahkan endpoint /ping

--- express-backend/index.js
+++ express-backend/index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const conn = require('./src/helpers/db')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+//import routes
+const pingRouter = require("./src/routes/pingRoute")
+
+app.use(bodyParser.json())
+
+//ping routes
+app.use('/', pingRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello World!??!')

342a8bd7b9b64a249ce6130e5d9cc89ca8375a32
RakaValrizqy <pendragonqword@gmail.com>
2025-08-06 10:44:17 +0700

    membuat helper response.js untuk format response

--- express-backend/index.js
+++ express-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const conn = require('./src/helpers/db')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 3030
 
 //import routes
 const pingRouter = require("./src/routes/pingRoute")

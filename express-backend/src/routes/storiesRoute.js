bc700c35367fc74ac4d04e5a9576cfd4435dd3bb
RakaValrizqy <pendragonqword@gmail.com>
2025-08-13 10:37:50 +0700

    menambahkan comment

--- express-backend/src/routes/storiesRoute.js
+++ express-backend/src/routes/storiesRoute.js
@@ -7,20 +7,22 @@ const upload = require('../middleware/uploadImage')
 
 app.use(express.json())
 
+//get all stories
 router.get('/stories', storyController.getStories)
-
 router.get('/stories/posted', storyController.getPostedStories)
 
+//get specific story
 router.get('/stories/:id', storyController.getStory)
-
 router.get('/stories/posted/:id', storyController.getPostedStory)
 
+//create story
 router.post('/stories', upload.array('images', 4), storyController.createStory)
 
+//update story
 router.put('/stories/:id', storyController.updateStory)
 
+//delete story (by status dan hard delete)
 router.put('/stories/:id/delete', storyController.deleteStoryByStatus)
-
 router.delete('/stories/:id', storyController.deleteStory)
 
 module.exports = router
